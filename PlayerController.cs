fd88a64dab578e748adce664f952fcefa4bdf82f
Michelle Slee <sleemichelle@gmail.com>
2020-11-01 11:40:13 +0000

    First commit

---
+++ PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour { 
+
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Move vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //Rotate vehicle left/right based on horizontalinput
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
---
+++ PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 50.0f;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    //Move forward
+    transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+     // get the user's vertical input
+     verticalInput = Input.GetAxis("Vertical");
+
+      //Tilt plane based on up/down arrows
+     transform.Rotate(Vector3.left, Time.deltaTime * rotationSpeed * verticalInput);
+
+     }
+}
